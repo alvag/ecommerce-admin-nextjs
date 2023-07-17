e9309547178ffa237af499faaae40b83eaa371a1
Max Alva Garcia <alva85@gmail.com>
2023-07-17 17:49:18 -0500

    Modal components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
+'use client';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+
 const SetupPage = () => {
+    const onOpen = useStoreModal( state => state.onOpen );
+    const isOpen = useStoreModal( state => state.isOpen );
+
+    useEffect( () => {
+        if ( !isOpen ) {
+            onOpen()
+        }
+    }, [ isOpen, onOpen ] );
+
+
     return (
         <div className="p-4">
-            this is a protected page
+            Root page
+
         </div>
     );
 };
